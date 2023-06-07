166f698a389f3b6ed602344ab46d2f05864e7814
KayeJin <pengjin250@gmail.com>
2023-06-07 16:48:28 +0800

    add interface

--- check.py
+++ check.py
@@ -17,73 +17,41 @@ from pyhanlp import *
 from math import ceil
 import time 
 import match
-import yyyy
 import db_class
 
-#取整个数据库 def（）
-def sensitive_word_recognize( database, table, columns, data, index, size): #data 数据，index 数据分片索引， size 进程数
-    data_len = len(data)
-    size = ceil(data_len/size)
-    start = index * size
-    end = (index + 1) * size if (index + 1) * size < data_len else data_len 
-    tmp_data = data[start : end]
-    result_str = ''
-    for j in tmp_data: #行分组块
-        k = 0
-        for i in j :
-            # 字段内容  “中风险”/“无风险”
-            # result = [database, table,columns[k],str(i), match2.auto_check_secret(str(i)) ]
-            result = [database, table,columns[k],str(i), match.auto_check_secret(str(i)) ]
-            result_str += str(result) + '\r\n' #对每个字段标识
-            k += 1
-    return result_str
     
      
 if __name__ == '__main__':
         
-        process_num = int(input('线程数： '))
-        #数据库
-        db = db_class.DB('localhost','debian-sys-maint','TTOn7ubVfTJJ89Y8','user_test')
-        #多线程------对数据块分线程
-        databases = db.get_database()  #  通过schemata获取所有数据库名称
-        start_time = time.time()
-        for database in databases:
-            tables = db.get_table(database) #  获取表名
-            for table in tables:
-                columns = db.get_column(database,table) #  获取字段名
-                data = db.get_line(database, table) #按行取数据
-                #读取数据库数据耗费的时间并不多，可能是因为只有一个表吧:D
-                print(len(data))
-                data[0:2]
-                res = [] 
-                # 线程
-                result_str = []
-                i = 0
-                with ThreadPoolExecutor(max_workers=process_num) as t:
-                    print('34134234234234')
-                    res = t.submit(lambda cxp:sensitive_word_recognize(*cxp),(database, table,columns, data, 0, 1)).result()
-                    print("-------------------------")
-                    result_str.append(res)
-        end_time = time.time()    
-        print(end_time - start_time)    
-        with open('message2.txt','a+',encoding='UTF-8') as file:
-            for result in result_str :
-                file.write(result)       
-            file.write("耗时：{:.2f}秒".format(end_time-start_time))    
+    process_num = int(input('线程数： '))
+    #数据库
+    db = db_class.DB('localhost','debian-sys-maint','TTOn7ubVfTJJ89Y8','user_test')
+    #多线程------对数据块分线程
+    databases = db.get_database()  #  通过schemata获取所有数据库名称
+    start_time = time.time()
+    for database in databases:
+        tables = db.get_table(database) #  获取表名
+        for table in tables:
+            columns = db.get_column(database,table) #  获取字段名
+            data = db.get_line(database, table) #按行取数据
+            #读取数据库数据耗费的时间并不多，可能是因为只有一个表吧:D
+            print(len(data))
+            # data[0:2]
+            res = [] 
+            # 线程
+            result_str = []
+            i = 0
+            with ThreadPoolExecutor(max_workers=process_num) as t:
+                res = t.submit(lambda cxp:match.sensitive_word_recognize(*cxp),(database, table,columns, data, 0, 1)).result()
+                print("-------------------------")
+                result_str.append(res)
+    end_time = time.time()    
+    print(end_time - start_time)    
+    with open('message2.txt','a+',encoding='UTF-8') as file:
+        for result in result_str :
+            file.write(result)       
+        file.write("耗时：{:.2f}秒".format(end_time-start_time))    
 
-
-        # 正则表达式
-        # bc = match2.check_secret(match2.bank_card_pattern, '我的手机号是18590085340,身份证是110101199003072957,银行卡是6225806592337329,我的邮箱是aka@gmail.com')
-        # print('银行卡',bc)
-        # bc = match2.check_secret(match2.moblie_phone_pattern, '我的手机号是18590085340,身份证是110101199003072957,银行卡是6225806592337329,我的邮箱是aka@gmail.com')
-        # print('手机',bc)
-        # bc = match2.check_secret(match2.id_pattern, '我的手机号是18590085340,身份证是110101199003072957,银行卡是6225806592337329,我的邮箱是aka@gmail.com')
-        # print('身份证',bc)
-        # bc = match2.check_secret(match2.email_pattern, '我的手机号是18590085340,身份证是110101199003072957,银行卡是6225806592337329,我的邮箱是aka@gmail.com')
-        # print('邮箱',bc)
-
-        # # Hanlp 自然语言处理
-        # address_list,name_list = match2.check_chinese_address_and_name('我家在北京市东城区东长安街, 我叫金翠花, 我的手机号是18590085340, 身份证是110101199003072957, 银行卡是6225806592337329, 我的邮箱是aka@gmail.com')
-        # print('地址', address_list)
-        # print('名字', name_list)
-        # print(match2.match('我家在北京市asljda@outlook.com东城区东长安街,gongzheng@qq.com 我叫金翠花, 我的手sdfsdfsd机号是18590085340, 身份证是110101199003072957, 银行卡是6225806592337329, 我的邮箱是aka@gmail.com,11010119900307295X'))
+    # bc = match.auto_check_secret(input('查询值： '))
+    # print(bc)
+        
--- match.py
+++ match.py
@@ -1,17 +1,19 @@
 import pymysql
 import re, sys
+from math import ceil
 import threading
 from pyhanlp import *
-# import match
 
 # http://www.360doc.com/content/21/0220/19/13664199_963059614.shtml
 moblie_phone_pattern = re.compile(r'1[356789]\d{9}')
-phone_pattern = re.compile(r'\d{3}-\d{8}|\d{4}-\d{7}') #固话
-id_pattern = re.compile(r'[1-9]\d{5}(?:18|19|(?:[23]\d))\d{2}(?:(?:0[1-9])|(?:10|11|12))(?:(?:[0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]')
+moblie_phone_pattern = re.compile(r'(13[0-9]|14[0145-9]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}')
+phone_pattern = re.compile(r'0\d{2,3}-[1-9]\d{6,7}') #固话
+id_pattern = re.compile(r'[1-9]\d{5}(?:18|19|(?:[23]\d))\d{2}(?:(?:0[1-9])|(?:10|11|12))(?:(?:[0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]')
 email_pattern = re.compile(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)')
 bank_card_pattern = re.compile(r'([1-9]{1}\d{15}|\d{18})')
 digit_pattern = re.compile(r'0|1|2|3|4|5|6|7|8|9')
-Address = r'(ns|nsf|nz)' #地址
+Address = r'(ns|nsf|nz|nt)' #地址
+
 Person_Name = r'nr|nrf' #人名
 
 CRFnewSegment = HanLP.newSegment("crf")  # 通过crf算法识别实体
@@ -21,33 +23,106 @@ s2 = "低风险"
 s3 = "中风险"
 s4 = "高风险"
 
+def sensitive_word_recognize( database, table, columns, data, index, size): #data 数据，index 数据分片索引， size 进程数
+    data_len = len(data)
+    size = ceil(data_len/size)
+    start = index * size
+    end = (index + 1) * size if (index + 1) * size < data_len else data_len 
+    tmp_data = data[start : end]
+    result_str = ''
+    for j in tmp_data: #行分组块
+        k = 0
+        for i in j :
+            # 字段内容  “中风险”/“无风险”
+            result = [database, table,columns[k],str(i), auto_check_secret(str(i))]
+            result_str += str(result) + '\r\n' #对每个字段标识
+            k += 1
+    return result_str
+
 def check_secret(pattern, value):
     return pattern.findall(value)
+
+'''
+def check_id(value):
+    if re.match(id_pattern, value):
+        return (value + ' 身份证 %s' % s4)
+    else :
+        return (value + ' 8 %s' % s1) 
+    
+def check_phone(value):
+    if re.match(phone_pattern, value):
+        return (value + ' 固话 %s' % s3)
+    else :
+        return (value + ' 8 %s' % s1)
+    
+def check_mobile_phone(value):
+    if re.match(moblie_phone_pattern,value) and len(value) == 11: #手机号 -- 高风险
+        return (value + ' 手机 %s' % s4)
+    else :
+        return (value + ' 8 %s' % s1)
+
+def check_email(value):
+    if re.match(email_pattern,value): #邮箱 -- 中风险
+        return (value + ' 邮箱 %s' % s3)
+    else :
+        return (value + ' 8 %s' % s1)
+
+
+def check_bank_card(value):
+    if re.match(bank_card_pattern,value): #银行卡  -- 中风险
+        return (value + ' 银行卡 %s' % s3)
+    else :
+        return (value + ' 8 %s' % s1)
+'''
+def check_chinese_address_and_name(value):
+    seg_text = CRFnewSegment.seg(value)
+
+    Address = r'(ns|nsf|nz|nt)'
+    #大学是 ntu
+    Person_Name = r'nr'  
+
+    dict = {}
+    address_list = []
+    name_list = []
+    
+    for i in seg_text:
+        dict[str(i.word)] = [str(i.nature)]
+    
+    for key, value in dict.items():
+        # print(key, value)
+        value = str(value)
+        if re.search(Address, value):
+            address_list.append(key)
+        if re.search(Person_Name, value):
+            name_list.append(key)
+    return address_list, name_list
+
 
 def auto_check_secret(value):
     address_list = [] #地址
     name_list = [] #姓名
 
+    if len(value) <= 1:
+        return (' 8 %s' % s1) # 无风险
+    
     if re.match(id_pattern,value): #身份证 -- 高风险
-        return (value + '3 %s' % s4)
+        return (value + ' 身份证 %s' % s4)
     elif re.match(bank_card_pattern,value): #银行卡  -- 中风险
-        return (value + '5 %s' % s3)
+        return (value + ' 银行卡 %s' % s3)
     elif re.match(phone_pattern,value): #固话 --- 中风险
-        return (value + '1 %s' % s3)
+        return (value + ' 固话 %s' % s3)
     elif re.match(email_pattern,value): #邮箱 -- 中风险
-        return (value + '4 %s' % s3)
-    elif re.match(moblie_phone_pattern,value): #手机号 -- 高风险
-        return (value + '2 %s' % s4)
+        return (value + ' 邮箱 %s' % s3)
+    elif re.match(moblie_phone_pattern,value) and len(value) == 11: #手机号 -- 高风险
+        return (value + ' 手机 %s' % s4)
     if ('\u4e00' <= value[0] <= '\u9fa5'): #中文字符 --- 字段
         address_list,name_list =check_chinese_address_and_name(value) #判别地址和姓名
         if address_list :
-            return (str(address_list) + '6 %s' % s4) #地址 -- 高风险
+            return (str(value) + ' 6 %s' % s4) #地址 -- 高风险
         if name_list :
-            return (str(name_list) + '7 %s' % s4) #姓名 -- 高风险
-    if value is None or re.match(digit_pattern,value):
-        return ('8 %s' % s1) # 无风险
+            return (str(value) + ' 7 %s' % s4) #姓名 -- 高风险
     sensitive_list = []
-    email,bank_card,phone,mobile_phone,id,address_list,name_list=match(value)
+    email,bank_card,phone,mobile_phone,id,address_list,name_list=sentence_match(value) #备注消息
     for column in [email,bank_card,phone] : # 中风险 -- 邮箱、银行卡、固话
             for key in column :
                 sensitive_list.append(key + ' %s' % s3) 
@@ -60,55 +135,58 @@ def auto_check_secret(value):
     else: 
         return ('8 %s' % s1) # 无风险
 
-def check_chinese_address_and_name(value):
-    seg_text = CRFnewSegment.seg(value)
+# def auto_check_secret(value):
+#     email_list,bank_card_list,phone_list,mobile_phone_list,id_list,address_list,name_list = sentence_match(value)
+#     result_list = []
+#     for email in email_list :
+#         result_list.append(email + ' 邮箱 %s' % s3)
+#     for bank_card in bank_card_list :
+#         result_list.append(bank_card + ' 银行卡 %s' % s3)
+#     for phone in phone_list :
+#         result_list.append(phone + ' 固话 %s' % s3)
+#     for mobile_phone in mobile_phone_list :
+#         result_list.append(mobile_phone + ' 手机 %s' % s4)
+#     for id in id_list :
+#         result_list.append(id + ' 身份证 %s' % s4)
+#     for address_list in address_list :
+        result_list
 
-    Address = r'(ns|nsf|nz)'
-    Person_Name = r'nr'
-    
-    dict = {}
-    
-    address_list = []
-    name_list = []
-    
-    for i in seg_text:
-        dict[str(i.word)] = [str(i.nature)]
-    
-    for key, value in dict.items():
-        # print(key, value)
-        # print("-------------")
-        value = str(value)
-        if re.search(Address, value):
-            address_list.append(key)
-        if re.search(Person_Name, value):
-            name_list.append(key)
-    return address_list, name_list
+
 
 def NameRecognize(sentence): #中国人名识别
     NER=HanLP.newSegment().enableNameRecognize(True)
     p_name=NER.seg(sentence)
     return p_name
 
-def match(sentence):
+def sentence_match(sentence):
+
     seg_text = NameRecognize(sentence)
     dict = {}
     for i in seg_text:
         dict[str(i.word)] = [str(i.nature)]
 
-    email = check_secret(email_pattern, sentence)
+    email = check_secret(email_pattern, sentence) #邮箱
 
-    # mobile_phone = check_secret(moblie_phone_pattern, sentence)
     phone = check_secret(phone_pattern, sentence) #固话
-    id = check_secret(id_pattern, sentence)
-    # bank_card = check_secret(bank_card_pattern, sentence)
+
+    # id = check_secret(id_pattern, sentence) #身份证
+
+    # mobile_phone = check_secret(moblie_phone_pattern, sentence) #手机
+
+    # bank_card = check_secret(bank_card_pattern, sentence) #银行卡
+
+
     #英文：nx
 
     address_list = []
     name_list = []
-    # id = []
-    phone = []
     mobile_phone = []
     bank_card = []
+    id = []
+    seg_text = NameRecognize(sentence)
+    dict = {}
+    for i in seg_text:
+        dict[str(i.word)] = [str(i.nature)]
 
     for key, value in dict.items():
         value = str(value)
@@ -117,10 +195,11 @@ def match(sentence):
         if re.search(Person_Name, value): #姓名 
             name_list.append(key)
 
-        if re.search(r'm', value):
-            if not True in [key in i for i in id]: # 不在身份证的数字
-                if re.match(bank_card_pattern,key): #银行卡  
-                    bank_card.append(key)
-                elif re.match(moblie_phone_pattern,key): #手机号 
-                    mobile_phone.append(key)
+        if re.search(r'm', value): #值为数字
+            if re.match(id_pattern, value):
+                id.append(key)
+            elif re.match(bank_card_pattern,key): #银行卡  
+                bank_card.append(key)
+            elif re.match(moblie_phone_pattern,key) : #手机号 
+                mobile_phone.append(key)
     return email,bank_card,phone,mobile_phone,id,address_list,name_list
