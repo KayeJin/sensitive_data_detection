dc422d8debb02050aae968f009b21a2ece1a6602
KayeJin <pengjin250@gmail.com>
2023-06-07 17:47:34 +0800

    add interface

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # sensitive_data_detection
+
+db_class.py
+处理数据库
+
+check.py
+判断接口
+
+match.py
+
+1. check\_实体名
+   如 check_id、check_phone、check_mobile_phone、check_email、check_bank_card、check_chinese_address_and_name 分别检查输入的字符串是否属于身份证、固话、手机号、邮箱、银行卡、中文地址与姓名
+
+2. check_secret
+   使用正则表达式的 findall()，找到所有输入参数中满足 pattern 的字符串
+
+3. sensitive_word_recognize
+   对数据块的所有字段进行识别，index 和 size 参数方便在调用该函数前 对数据块分块
+
+4. auto_check_secret
+   自动识别敏感数据，可以识别单个实体与一段话
+
+5. NameRecognize
+   使用 Hanlp 进行分词
+
+6. sentence_match
+   识别一段话中的敏感消息
--- match.py
+++ match.py
@@ -8,7 +8,8 @@ from pyhanlp import *
 moblie_phone_pattern = re.compile(r'1[356789]\d{9}')
 # moblie_phone_pattern = re.compile(r'(13[0-9]|14[0145-9]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}')
 phone_pattern = re.compile(r'0\d{2,3}-[1-9]\d{6,7}') #固话
-id_pattern = re.compile(r'[1-9]\d{5}(?:18|19|(?:[23]\d))\d{2}(?:(?:0[1-9])|(?:10|11|12))(?:(?:[0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]')
+# id_pattern = re.compile(r'[1-9]\d{5}(?:18|19|(?:[23]\d))\d{2}(?:(?:0[1-9])|(?:10|11|12))(?:(?:[0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]')#18位身份证
+id_pattern = re.compile(r'([1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx])|([1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2})')
 email_pattern = re.compile(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)')
 bank_card_pattern = re.compile(r'([1-9]{1}\d{15}|\d{18})')
 digit_pattern = re.compile(r'0|1|2|3|4|5|6|7|8|9')
@@ -67,7 +68,6 @@ def check_email(value):
     else :
         return (value + ' 8 %s' % s1)
 
-
 def check_bank_card(value):
     if re.match(bank_card_pattern,value): #银行卡  -- 中风险
         return (value + ' 银行卡 %s' % s3)
@@ -101,13 +101,13 @@ def check_chinese_address_and_name(value):
 def auto_check_secret(value):
     address_list = [] #地址
     name_list = [] #姓名
-
+#and len(value) == 18
     if len(value) <= 1:
         return (' 8 %s' % s1) # 无风险
     
-    if re.match(id_pattern,value) and len(value) == 18 : #身份证 -- 高风险
+    if re.match(id_pattern,value)  : #身份证 -- 高风险
         return (value + ' 身份证 %s' % s4)
-    elif re.match(bank_card_pattern,value) and len(value) == 16: #银行卡  -- 中风险
+    elif re.match(bank_card_pattern,value) and len(value) == (15 or 18): #银行卡  -- 中风险
         return (value + ' 银行卡 %s' % s3)
     elif re.match(phone_pattern,value): #固话 --- 中风险
         return (value + ' 固话 %s' % s3)
@@ -115,12 +115,13 @@ def auto_check_secret(value):
         return (value + ' 邮箱 %s' % s3)
     elif re.match(moblie_phone_pattern,value) and len(value) == 11: #手机号 -- 高风险
         return (value + ' 手机 %s' % s4)
-    if ('\u4e00' <= value[0] <= '\u9fa5'): #中文字符 --- 字段
-        address_list,name_list =check_chinese_address_and_name(value) #判别地址和姓名
-        if address_list :
-            return (str(value) + ' 地址 %s' % s4) #地址 -- 高风险
-        if name_list :
-            return (str(value) + ' 姓名 %s' % s4) #姓名 -- 高风险
+    # if ('\u4e00' <= value[0] <= '\u9fa5'): #中文字符 --- 字段
+    #     address_list,name_list =check_chinese_address_and_name(value) #判别地址和姓名
+    #     if address_list :
+    #         return (str(value) + ' 地址 %s' % s4) #地址 -- 高风险
+    #     if name_list :
+    #         return (str(value) + ' 姓名 %s' % s4) #姓名 -- 高风险
+    
     sensitive_list = []
     email,bank_card,phone,mobile_phone,id,address_list,name_list=sentence_match(value) #备注消息
     for column in [email,bank_card,phone] : # 中风险 -- 邮箱、银行卡、固话
@@ -137,7 +138,7 @@ def auto_check_secret(value):
 
 
 
-def NameRecognize(sentence): #中国人名识别
+def NameRecognize(sentence): #识别
     NER=HanLP.newSegment().enableNameRecognize(True)
     p_name=NER.seg(sentence)
     return p_name
@@ -146,6 +147,7 @@ def sentence_match(sentence):
 
     seg_text = NameRecognize(sentence)
     dict = {}
+    # print(seg_text)
     for i in seg_text:
         dict[str(i.word)] = [str(i.nature)]
 
@@ -173,7 +175,7 @@ def sentence_match(sentence):
             name_list.append(key)
 
         if re.search(r'm', value): #值为数字
-            if re.match(id_pattern, value) and len(value) == 18: #身份证
+            if re.match(id_pattern, value) and len(value) == (15 or 18): #身份证
                 id.append(key)
             elif re.match(bank_card_pattern,key) and len(value) == 16: #银行卡  
                 bank_card.append(key)
